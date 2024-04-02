abb0df5ad123bebd2da990caeee1da799d9ee55b
Adam Michałowski <adam.michalowski@aexol.com>
2024-04-02 21:07:12 +0200

    changed input ring css

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
   const inputClassName = cn(
-    'flex h-10 w-full max-w-sm rounded-md border border-stone-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-stone-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-stone-800 dark:bg-stone-950 dark:ring-offset-stone-950 dark:placeholder:text-stone-400 dark:focus-visible:ring-stone-300',
+    'flex h-10 w-full max-w-sm rounded-md border border-stone-200 bg-white px-3 py-2 text-sm flex file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-stone-500 focus-visible:outline-none focus-visible:border-stone-950 dark:focus-visible:border-stone-500  disabled:cursor-not-allowed disabled:opacity-50 dark:border-stone-800 dark:bg-stone-950 dark:placeholder:text-stone-400',
     className,
   );
   if (props.label) {
